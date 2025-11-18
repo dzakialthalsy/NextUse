1146b212e819cdb5871f22debb91ee765dbe17b5
Dzaki Althalsyah <dz4ic7@gmail.com>
2025-11-18 20:34:19 +0700

    fix error di beranda

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -33,7 +33,6 @@
                         <div class="p-3 text-sm text-gray-500">Tidak ada notifikasi</div>
                     </div>
                 </div>
-                <div class="w-8 h-8 rounded-full main-gradient flex items-center justify-center">
                     <svg class="w-4 h-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z" /></svg>
                 </a>
             </div>
